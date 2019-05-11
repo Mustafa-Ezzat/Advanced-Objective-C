fd557db02701f8dd26588e9d12565ec037580309
Mustafa Ezzat <mezzat@waqood.sa>
2019-05-12 00:34:48 +0200

    Stack
    Stack

---
+++ Data Structure/Stack/Stack/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Stack
+//
+//  Created by Mustafa Ezzat on 5/12/19.
+//  Copyright © 2019 Waqood. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
