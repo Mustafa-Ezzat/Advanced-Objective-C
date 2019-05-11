dc4537bf5c06acdc4abd7d3cfef4a12246191e88
Mustafa Ezzat <mezzat@waqood.sa>
2019-05-12 00:47:06 +0200

    Stack implementation
    Stack implementation

--- Data Structure/Stack/Stack/main.m
+++ Data Structure/Stack/Stack/main.m
@@ -8,10 +8,53 @@
 
 #import <Foundation/Foundation.h>
 
+@interface Stack: NSObject
+@property NSMutableArray *data;
+-(void)push: (id)element;
+-(BOOL)pop;
+-(id)top;
+-(BOOL)isEmpty;
+@end
+
+@implementation Stack
+@synthesize data;
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        data = [[NSMutableArray alloc] init];
+    }
+    return self;
+}
+- (void)push:(id)element {
+    [data addObject:element];
+}
+-(BOOL)pop {
+    if([self isEmpty]) {
+        return false;
+    }
+    [data removeLastObject];
+    return true;
+}
+-(id)top {
+    return [data lastObject];
+}
+- (BOOL)isEmpty {
+    return ![data count];
+}
+@end
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!");
+        Stack *stack = [[Stack alloc] init];
+        [stack push:[[NSNumber alloc] initWithInt:-128]];
+        [stack push:[[NSNumber alloc] initWithInt:127]];
+        [stack pop];
+        NSNumber *top = [stack top];
+        NSLog(@"Top: %@", top);
+        [stack pop];
+        [stack isEmpty];
     }
     return 0;
 }
